a12c8f048b48b1f8f7e7f28a38fb794bb7d7c593
SoraiaFreire <freiresoraia00@gmail.com>
2026-06-19 08:35:57 -0300

    CloudKit

--- Quiclick/Model/WorkoutModel.swift
+++ Quiclick/Model/WorkoutModel.swift
@@ -10,11 +10,11 @@ import SwiftData
 @Model
 class WorkoutModel: Identifiable{
     
-    var id : UUID
-    var date: Date
-    var duration: TimeInterval
-    var distance: Double
-    var pace : Double
+    var id : UUID?
+    var date: Date = Date()
+    var duration: TimeInterval = 0.0
+    var distance: Double  = 0.0
+    var pace : Double = 0.0
     var imageData: Data?
     
     
